934636264541f72a40e1db48bea64a0b06145238
arturwyroslak <95839215+arturwyroslak@users.noreply.github.com>
2023-09-02 21:58:46 +0200

    Update Clipboard.tsx

--- src/components/Clipboard.tsx
+++ src/components/Clipboard.tsx
@@ -5,7 +5,7 @@ export default function Clipboard(props: { message: string }) {
   const [copied, setCopied] = createSignal(false)
   return (
     <button
-      title="复制"
+      title="Utwórz kopię"
       classList={{
         copied: copied(),
         copy: true,
