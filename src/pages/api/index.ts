008efb9559cfb58789ca6da738287db98812c02d
arturwyroslak <95839215+arturwyroslak@users.noreply.github.com>
2023-09-02 23:27:18 +0200

    Update index.ts

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,54 +1,73 @@
-import type { APIRoute } from "astro"
+import type { APIRoute } from "astro";
+import { checkForPluginOperation, communicateWithOpenAI } from './helperFunctions'; // Zaimportuj funkcje pomocnicze
 
 export const post: APIRoute = async ({ request }) => {
-  const { message, key } = (await request.json()) ?? {}
+  const { message, key } = (await request.json()) ?? {};
+
   if (!message) {
     return {
       body: JSON.stringify({
         success: false,
         message: "message is required"
       })
-    }
+    };
   }
+
   if (!key) {
     return {
       body: JSON.stringify({
         success: false,
         message: "openapi key is required"
       })
-    }
+    };
   }
 
-  const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${key}`,
-      "Content-Type": `application/json`
-    },
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content: message
-        }
-      ]
-    })
-  })
-  let result = await response.json()
-  if (result?.error) {
+  const canMatchToPlugin = await checkForPluginOperation(message);
+
+  if (canMatchToPlugin) {
+    const pluginResponse = await communicateWithOpenAI(message, key);
     return {
       body: JSON.stringify({
-        success: false,
-        message: `${result.error?.message}`
+        success: true,
+        message: "ok",
+        data: pluginResponse
       })
+    };
+  } else {
+    const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${key}`,
+        "Content-Type": `application/json`
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: message
+          }
+        ]
+      })
+    });
+
+    let result = await response.json();
+
+    if (result?.error) {
+      return {
+        body: JSON.stringify({
+          success: false,
+          message: `${result.error?.message}`
+        })
+      };
     }
+
+    return {
+      body: JSON.stringify({
+        success: true,
+        message: "ok",
+        data: result?.choices?.[0].message
+      })
+    };
   }
-  return {
-    body: JSON.stringify({
-      success: true,
-      message: "ok",
-      data: result?.choices?.[0].message
-    })
-  }
-}
+};
